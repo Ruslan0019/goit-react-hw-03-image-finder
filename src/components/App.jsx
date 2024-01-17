09b0560bbecdc5244b0ff15f5c5fa16cf357ecde
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-01-17 21:56:48 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
+import fetchData from './api/api';
 
 class App extends Component {
   state = {
@@ -14,37 +15,53 @@ class App extends Component {
     modalImageUrl: null,
   };
 
+  componentDidMount() {
+    const { query } = this.state;
+    if (query.trim() !== '') {
+      this.fetchData(query, 1);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if ((prevState.query !== query && page === 1) || prevState.page !== page) {
+    }
+  }
+
   handleSubmit = newQuery => {
-    this.setState({
-      query: newQuery,
-      images: [],
-      page: 1,
-    });
-    this.fetchData(newQuery, 1);
+    this.setState(
+      {
+        query: newQuery,
+        images: [],
+        page: 1,
+        loading: true,
+      },
+      () => {
+        this.fetchData(newQuery, 1);
+      }
+    );
   };
 
   fetchData = async (query, pageNumber) => {
     try {
-      this.setState({ loading: true });
+      const data = await fetchData(query, pageNumber);
 
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=40489521-2d233b9ce133180f8f85686cd&image_type=photo&orientation=horizontal&per_page=12`
-      );
-
-      const data = await response.json();
       this.setState(prevState => ({
-        images: [...prevState.images, ...data.hits],
+        images: [...prevState.images, ...data],
         page: prevState.page + 1,
+        loading: false,
       }));
     } catch (error) {
       console.error('Error while receiving data:', error);
-    } finally {
       this.setState({ loading: false });
     }
   };
 
   loadMore = () => {
-    this.fetchData(this.state.query, this.state.page);
+    const { query, page } = this.state;
+    this.setState({ loading: true }, () => {
+      this.fetchData(query, page);
+    });
   };
 
   openModal = imageUrl => {
@@ -54,14 +71,19 @@ class App extends Component {
   closeModal = () => {
     this.setState({ modalImageUrl: null });
   };
+
   render() {
     const { images, loading, modalImageUrl } = this.state;
+    const allImagesLoaded = images.length === 0 && !loading;
+
     return (
       <div>
         <SearchBar onSubmit={this.handleSubmit} />
         <ImageGallery images={images} onImageClick={this.openModal} />
         {loading && <Loader />}
-        {images.length > 0 && <Button onClick={this.loadMore} />}
+        {images.length >= 12 && !allImagesLoaded && (
+          <Button onClick={this.loadMore} />
+        )}
         {modalImageUrl && (
           <Modal imageUrl={modalImageUrl} onClose={this.closeModal} />
         )}
@@ -69,4 +91,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
